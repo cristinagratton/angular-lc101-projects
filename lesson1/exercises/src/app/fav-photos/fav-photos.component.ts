cada28bb325c02fdf1016e6349b3c9148daec46c
Cristina <cristina.gratton@gmail.com>
2021-12-06 12:34:18 -0600

    part 2

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Favorite Photos';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+  image1 = 'https://illinois.edu/assets/img/navigation/submenu_about.jpg';
   image2 = 'https://i.ytimg.com/vi/FcvJDKlF9_s/maxresdefault.jpg';
   image3 = 'https://d20t6we9nb6rnn.cloudfront.net/cristinaandsteve/1528837046095-300x300.jpg';
+  image4 = 'https://media2.giphy.com/media/xT1XGzXhVgWRLN1Cco/giphy.gif?cid=ecf05e475vdqoh1sk8a64hblpcpps2xgxqymrefo9gfsn1yv&rid=giphy.gif&ct=g';
 
   constructor() { }
 
