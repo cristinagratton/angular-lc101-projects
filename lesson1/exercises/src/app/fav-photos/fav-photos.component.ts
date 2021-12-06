29ccfbcd83b9516f41642edd2b4aa865d2cfd983
Cristina <cristina.gratton@gmail.com>
2021-12-06 12:10:59 -0600

    part 1 exercise 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i.ytimg.com/vi/FcvJDKlF9_s/maxresdefault.jpg';
+  image3 = 'https://d20t6we9nb6rnn.cloudfront.net/cristinaandsteve/1528837046095-300x300.jpg';
 
   constructor() { }
 
